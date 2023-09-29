70772d699b0332e4e364697abe35d13e9351b6ab
zerojin <qser155@naver.com>
2023-09-29 14:44:49 +0900

    canvas clock 구현

--- canvase_그리기/index.js
+++ canvase_그리기/index.js
@@ -1,22 +1,39 @@
 var canvas = document.querySelector('#canvas');
 var ctx = canvas.getContext('2d');
-var isDraw = false;
-var startDraw = function (e) {
-    ctx.beginPath();
-    ctx.moveTo(e.offsetX, e.offsetY);
-    ctx.fillStyle = 'black';
-    isDraw = true;
-};
-var drawing = function (e) {
-    if (isDraw) {
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
+drawClock();
+function drawClock() {
+    drawFace(ctx, 200);
+    drawNumbers(ctx, 200);
+}
+function drawNumbers(ctx, radius) {
+    ctx.font = radius * 0.15 + 'px arial';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    for (var num = 1; num < 13; num++) {
+        var ang = (num * Math.PI) / 6;
+        ctx.rotate(ang);
+        ctx.translate(50, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(50, radius * 0.85);
+        ctx.rotate(-ang);
     }
-};
-var endDrawing = function (e) {
-    ctx.closePath();
-    isDraw = false;
-};
-canvas.addEventListener('mousedown', startDraw);
-canvas.addEventListener('mousemove', drawing);
-canvas.addEventListener('mouseup', endDrawing);
+}
+function drawFace(ctx, radius) {
+    var grad = ctx.createRadialGradient(250, 250, radius * 0.95, 250, 250, radius * 1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    ctx.beginPath();
+    ctx.arc(250, 250, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(250, 250, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
--- canvase_그리기/index.ts
+++ canvase_그리기/index.ts
@@ -1,27 +1,53 @@
 let canvas: HTMLCanvasElement = document.querySelector('#canvas')!;
 let ctx = canvas.getContext('2d')!;
-let isDraw: boolean = false;
-// isDraw를 명시하지 않으면 mousemove 에서 영역 내로 들어가면 바로 그림을 그려버림
 
-const startDraw = (e: MouseEvent) => {
-    ctx.beginPath();
-    ctx.moveTo(e.offsetX, e.offsetY);
-    ctx.fillStyle = 'black';
-    isDraw = true;
-};
+drawClock();
 
-const drawing = (e: MouseEvent) => {
-    if (isDraw) {
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
+function drawClock() {
+    drawFace(ctx, 200);
+    drawNumbers(ctx, 200);
+}
+
+function drawNumbers(ctx, radius) {
+    ctx.font = radius * 0.15 + 'px arial';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    for (let num = 1; num < 13; num++) {
+        let ang = (num * Math.PI) / 6;
+        ctx.rotate(ang);
+        ctx.translate(50, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(50, radius * 0.85);
+        ctx.rotate(-ang);
     }
-};
+}
 
-const endDrawing = (e: MouseEvent) => {
-    ctx.closePath();
-    isDraw = false;
-};
+function drawFace(ctx, radius) {
+    const grad = ctx.createRadialGradient(
+        250,
+        250,
+        radius * 0.95,
+        250,
+        250,
+        radius * 1.05
+    );
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
 
-canvas.addEventListener('mousedown', startDraw);
-canvas.addEventListener('mousemove', drawing);
-canvas.addEventListener('mouseup', endDrawing);
+    ctx.beginPath();
+    ctx.arc(250, 250, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(250, 250, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
