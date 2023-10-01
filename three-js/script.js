23347f8f8576722f8f70b27080ba8cd2a00b936a
zerojin <qser155@naver.com>
2023-10-01 14:15:32 +0900

    라이브러리 gsap 를 이용한 animation 효과

--- three-js/script.js
+++ three-js/script.js
@@ -1,72 +1,33 @@
 import * as THREE from 'three';
+
+import gsap from 'gsap';
+
 const scene = new THREE.Scene();
 const canvas = document.querySelector('canvas.webgl');
 
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-// const material = new THREE.MeshBasicMaterial({ color: 'rgb(255, 0, 0)' });
+const material = new THREE.MeshBasicMaterial({ color: 'rgb(255, 0, 0)' });
 
-// const mesh = new THREE.Mesh(geometry, material);
+const mesh = new THREE.Mesh(geometry, material);
 
-// scene.add(mesh);
+scene.add(mesh);
+
+const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height);
+
+camera.position.set(0, -1, 5);
+scene.add(camera);
+
 const renderer = new THREE.WebGLRenderer({
     canvas,
 });
 
 renderer.setSize(800, 600);
 
-const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height);
-
-camera.position.x = 1;
-camera.position.y = 1;
-camera.position.z = 3;
-scene.add(camera);
-
-const group = new THREE.Group();
-group.scale.y = 2;
-group.rotation.y = 0.2;
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube1.position.x = -1.5;
-group.add(cube1);
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-);
-cube2.position.x = 0;
-group.add(cube2);
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
-);
-cube3.position.x = 1.5;
-group.add(cube3);
-
-group.children.forEach((child, idx) => {
-    child.position.set(idx, 0, 0);
-});
-
-camera.position.set(0, 0, 5);
-
-const clock = new THREE.Clock();
-
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-
-    group.rotation.y = Math.PI * elapsedTime;
-    group.position.x = Math.cos(elapsedTime);
-    group.position.y = Math.sin(elapsedTime);
-
-    camera.lookAt(group.position);
-
-    window.requestAnimationFrame(tick);
+    gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
     renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
 };
 
 tick();
