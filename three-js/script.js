f3f2b9af29a7d31c6ab00a33da94144b6e8e1d32
zerojin <qser155@naver.com>
2023-10-01 15:59:47 +0900

    OribitControls를 이용해서 scroll zoom

--- three-js/script.js
+++ three-js/script.js
@@ -1,57 +1,68 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-import gsap from 'gsap';
+const canvas = document.querySelector('canvas.webgl');
 
+// Sizes
 const sizes = {
     width: 800,
     height: 600,
 };
 
+const cursor = {
+    x: 0,
+    y: 0,
+};
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+});
+
 const scene = new THREE.Scene();
-const canvas = document.querySelector('canvas.webgl');
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-const material = new THREE.MeshBasicMaterial({ color: 'rgb(255, 0, 0)' });
-
-const mesh = new THREE.Mesh(geometry, material);
-
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
 scene.add(mesh);
 
+// Camera
 const camera = new THREE.PerspectiveCamera(
     75,
     sizes.width / sizes.height,
     0.1,
     100
 );
-
-camera.position.set(0, 0, 5);
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
+camera.position.z = 3;
 scene.add(camera);
 
-const renderer = new THREE.WebGLRenderer({
-    canvas,
-});
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
 renderer.setSize(sizes.width, sizes.height);
 
-const cursor = {
-    x: 0,
-    y: 0,
-};
-window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = -(event.clientY / sizes.height - 0.5);
-    tick();
-});
+// Animate
+const clock = new THREE.Clock();
 
 const tick = () => {
-    // gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-    camera.position.y = cursor.y * 3;
-    camera.lookAt(mesh.position);
+    const elapsedTime = clock.getElapsedTime();
 
+    // Update controls
+    controls.update();
+
+    // Render
     renderer.render(scene, camera);
-    // window.requestAnimationFrame(tick);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
 };
+
 tick();
