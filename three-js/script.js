999d0f8b3844d5f474f2ffd50d3eb1563e7afa9c
zerojin <qser155@naver.com>
2023-10-01 15:03:40 +0900

    마우스 위치에 따라 카메라  이동

--- three-js/script.js
+++ three-js/script.js
@@ -2,6 +2,11 @@ import * as THREE from 'three';
 
 import gsap from 'gsap';
 
+const sizes = {
+    width: 800,
+    height: 600,
+};
+
 const scene = new THREE.Scene();
 const canvas = document.querySelector('canvas.webgl');
 
@@ -13,21 +18,40 @@ const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
 
-const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height);
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
 
-camera.position.set(0, -1, 5);
+camera.position.set(0, 0, 5);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
     canvas,
 });
 
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
+
+const cursor = {
+    x: 0,
+    y: 0,
+};
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+    tick();
+});
 
 const tick = () => {
-    gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(tick);
-};
+    // gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+    camera.position.y = cursor.y * 3;
+    camera.lookAt(mesh.position);
 
+    renderer.render(scene, camera);
+    // window.requestAnimationFrame(tick);
+};
 tick();
